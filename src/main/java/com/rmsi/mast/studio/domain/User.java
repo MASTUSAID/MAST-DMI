b7b7b6be686ee93edba4a7d68865cf2ce9d1659a
Alexander Solovov <a_solovov@yahoo.com>
2019-08-14 12:09:14 +0600

    Changes 14.08.2019

--- src/main/java/com/rmsi/mast/studio/domain/User.java
+++ src/main/java/com/rmsi/mast/studio/domain/User.java
@@ -27,250 +27,230 @@ public class User implements Serializable {
 
     //@Column(name="userid",insertable=false, updatable=false)
     //private long id;
-	@Id
+    @Id
     @SequenceGenerator(name = "USER_ID_GENERATOR", sequenceName = "la_ext_user_userid_seq", allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "USER_ID_GENERATOR")
     @Column(name = "userid", insertable = false, updatable = false, unique = true, nullable = false)
-	private long id;
-	
+    private long id;
+
     private String name;
-	@Column(name="isactive")
+    @Column(name = "isactive")
     private Boolean active;
-	//private String defaultproject;
-	@Column(name="emailid")
+    //private String defaultproject;
+    @Column(name = "emailid")
     private String email;
-	
-	@Column(name="contactno")
+
+    @Column(name = "contactno")
     private String phone;
-    
+
     private String password;
-    
-    @Column(name="authenticationkey")
+
+    @Column(name = "authenticationkey")
     private String authkey;
-	
-    @Column(name="managername")
+
+    @Column(name = "managername")
     private String manager_name;
     private String username;
-    
+
     private Long createdby;
-	private Long modifiedby;
-	private Date createddate;
+    private Long modifiedby;
+    private Date createddate;
     private Date modifieddate;
     @JsonSerialize(using = JsonDateSerializer.class)
-    private Date passwordexpires ;
-    private Date lastactivitydate ;
-    
-    
+    private Date passwordexpires;
+    private Date lastactivitydate;
+
     private String defaultproject;
-	
+
     public Date getPasswordexpires() {
-		return passwordexpires;
-	}
-
-	public void setPasswordexpires(Date passwordexpires) {
-		this.passwordexpires = passwordexpires;
-	}
-
-	public Date getLastactivitydate() {
-		return lastactivitydate;
-	}
-
-	public void setLastactivitydate(Date lastactivitydate) {
-		this.lastactivitydate = lastactivitydate;
-	}
-
-	@Column(name="address")
-	private String address;
-	
-	@Transient
-	 private String reportingTo;
-	    
-	 
-	@Transient
-	private  Set<Project> project;
-	
-	
-  
-
-    public String getReportingTo() {
-		return reportingTo;
-	}
-
-	
-	@Column(name="genderid")
-	private int gender;
-    
-   
-    
- 
-    @OneToMany(mappedBy="user",cascade = CascadeType.ALL,fetch = FetchType.EAGER)
-	private Set<UserRole> userRole;
-
-	
- 
-	/*@OneToMany(mappedBy="user",cascade = CascadeType.ALL,fetch = FetchType.EAGER)
-	private Set<UserProject> userProject;
-*/
-	
-   
-	public User() {
+        return passwordexpires;
     }
 
-	public long getId() {
-		return id;
-	}
+    public void setPasswordexpires(Date passwordexpires) {
+        this.passwordexpires = passwordexpires;
+    }
 
-	public int getGender() {
-		return gender;
-	}
+    public Date getLastactivitydate() {
+        return lastactivitydate;
+    }
 
-	public void setGender(int gender) {
-		this.gender = gender;
-	}
+    public void setLastactivitydate(Date lastactivitydate) {
+        this.lastactivitydate = lastactivitydate;
+    }
 
-	public void setId(long id) {
-		this.id = id;
-	}
+    @Column(name = "address")
+    private String address;
 
-	public String getName() {
-		return name;
-	}
+    @Transient
+    private String reportingTo;
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    @Transient
+    private Set<Project> project;
 
-	public Boolean getActive() {
-		return active;
-	}
+    public String getReportingTo() {
+        return reportingTo;
+    }
 
-	public void setActive(Boolean active) {
-		this.active = active;
-	}
+    @Column(name = "genderid")
+    private int gender;
 
-	
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private Set<UserRole> userRole;
 
-	public String getEmail() {
-		return email;
-	}
+    /*@OneToMany(mappedBy="user",cascade = CascadeType.ALL,fetch = FetchType.EAGER)
+	private Set<UserProject> userProject;
+     */
+    public User() {
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public long getId() {
+        return id;
+    }
 
-	public String getPhone() {
-		return phone;
-	}
+    public int getGender() {
+        return gender;
+    }
 
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
+    public void setGender(int gender) {
+        this.gender = gender;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public void setId(long id) {
+        this.id = id;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public String getAuthkey() {
-		return authkey;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public void setAuthkey(String authkey) {
-		this.authkey = authkey;
-	}
+    public Boolean getActive() {
+        return active;
+    }
 
-	public String getManager_name() {
-		return manager_name;
-	}
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
 
-	public void setManager_name(String manager_name) {
-		this.manager_name = manager_name;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	public String getUsername() {
-		return username;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	public void setUsername(String username) {
-		this.username = username;
-	}
+    public String getPhone() {
+        return phone;
+    }
 
-	public String getAddress() {
-		return address;
-	}
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 
-	public void setAddress(String address) {
-		this.address = address;
-	}
+    public String getPassword() {
+        return password;
+    }
 
-	
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-	public Long getCreatedby() {
-		return createdby;
-	}
+    public String getAuthkey() {
+        return authkey;
+    }
 
-	public void setCreatedby(Long createdby) {
-		this.createdby = createdby;
-	}
+    public void setAuthkey(String authkey) {
+        this.authkey = authkey;
+    }
 
-	public Long getModifiedby() {
-		return modifiedby;
-	}
+    public String getManager_name() {
+        return manager_name;
+    }
 
-	public void setModifiedby(Long modifiedby) {
-		this.modifiedby = modifiedby;
-	}
+    public void setManager_name(String manager_name) {
+        this.manager_name = manager_name;
+    }
 
-	public Date getCreateddate() {
-		return createddate;
-	}
+    public String getUsername() {
+        return username;
+    }
 
-	public void setCreateddate(Date createddate) {
-		this.createddate = createddate;
-	}
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-	public Date getModifieddate() {
-		return modifieddate;
-	}
+    public String getAddress() {
+        return address;
+    }
 
-	public void setModifieddate(Date modifieddate) {
-		this.modifieddate = modifieddate;
-	}
+    public void setAddress(String address) {
+        this.address = address;
+    }
 
-	public Set<UserRole> getUserRole() {
-		return userRole;
-	}
+    public Long getCreatedby() {
+        return createdby;
+    }
 
-	public void setUserRole(Set<UserRole> userRole) {
-		this.userRole = userRole;
-	}
+    public void setCreatedby(Long createdby) {
+        this.createdby = createdby;
+    }
 
-	public void setReportingTo(String reportingTo) {
-		this.reportingTo = reportingTo;
-	}
+    public Long getModifiedby() {
+        return modifiedby;
+    }
 
-	public String getDefaultproject() {
-		return defaultproject;
-	}
+    public void setModifiedby(Long modifiedby) {
+        this.modifiedby = modifiedby;
+    }
 
-	public void setDefaultproject(String defaultproject) {
-		this.defaultproject = defaultproject;
-	}
+    public Date getCreateddate() {
+        return createddate;
+    }
 
-	@Transient
-	public Set<Project> getProject() {
-		return project;
-	}
+    public void setCreateddate(Date createddate) {
+        this.createddate = createddate;
+    }
 
-	@Transient
-	public void setProject(Set<Project> project) {
-		this.project = project;
-	}
+    public Date getModifieddate() {
+        return modifieddate;
+    }
 
-	
-	
-    
+    public void setModifieddate(Date modifieddate) {
+        this.modifieddate = modifieddate;
+    }
+
+    public Set<UserRole> getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(Set<UserRole> userRole) {
+        this.userRole = userRole;
+    }
+
+    public void setReportingTo(String reportingTo) {
+        this.reportingTo = reportingTo;
+    }
+
+    public String getDefaultproject() {
+        return defaultproject;
+    }
+
+    public void setDefaultproject(String defaultproject) {
+        this.defaultproject = defaultproject;
+    }
+
+    @Transient
+    public Set<Project> getProject() {
+        return project;
+    }
+
+    @Transient
+    public void setProject(Set<Project> project) {
+        this.project = project;
+    }
+
 }
